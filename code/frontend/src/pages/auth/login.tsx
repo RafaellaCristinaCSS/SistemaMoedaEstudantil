c22b95b76b043eaf1618f6761c732c95dbd1eef3
BelleNerissa <belizeu@sga.pucminas.com>
2023-11-28 20:39:16 -0300

    refatoração login

--- code/frontend/src/pages/auth/login.tsx
+++ code/frontend/src/pages/auth/login.tsx
@@ -1,14 +1,71 @@
-import { Button, Checkbox, Form, Input } from "antd";
+import React from "react";
+import { Button, Form, Input } from "antd";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const App: React.FC = () => {
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+interface AuthResponse {
+  access_token: string;
+}
+
+interface UserInfo {
+  id: string;
+  role: string;
+}
+
+const LoginForm: React.FC<{
+  onFinish: (values: LoginFormValues) => Promise<AuthResponse | undefined>;
+  onFinishFailed: (errorInfo: any) => void;
+}> = ({ onFinish, onFinishFailed }) => {
+  return (
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          { required: true, message: "Please input your email!" },
+          { type: "email", message: "Please enter a valid email address!" },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+const AuthPage: React.FC = () => {
   const router = useRouter();
-  const OnFinish = async (values: any) => {
+
+  const handleAuth = async (values: LoginFormValues) => {
     try {
       const response = await axios.post("http://localhost:5500/auth", values);
-
       localStorage.setItem("token", response.data.access_token);
 
       const headers = {
@@ -16,19 +73,12 @@ const App: React.FC = () => {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
       };
 
-      const userInfo: any = await axios.get("http://localhost:5500/auth/info", {
-        headers,
-      });
+      const userInfo: UserInfo = await axios.get(
+        "http://localhost:5500/auth/info",
+        { headers }
+      );
 
-      localStorage.setItem("id", userInfo.data.id);
-
-      if (userInfo.data.role === "company") {
-        router.push(`/company`);
-      } else if (userInfo.data.role === "student") {
-        router.push(`/student`);
-      } else if (userInfo.data.role === "teacher") {
-        router.push(`/teacher`);
-      }
+      localStorage.setItem("id", userInfo.id);
 
       return response.data;
     } catch (error) {
@@ -36,56 +86,35 @@ const App: React.FC = () => {
     }
   };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
+  const onFinish = async (values: LoginFormValues) => {
+    const authResponse = await handleAuth(values);
+
+    if (authResponse) {
+      const { role } = await handleAuth(values);
+
+      if (role === "company") {
+        router.push(`/company`);
+      } else if (role === "student") {
+        router.push(`/student`);
+      } else if (role === "teacher") {
+        router.push(`/teacher`);
+      }
+    }
   };
 
-  type FieldType = {
-    email?: string;
-    password?: string;
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
   };
 
   return (
     <div className="forms formsGeral">
       <h1 className="title">Login:</h1>
-
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={OnFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="email"
-          name="email"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-
-        <div>
-          <Link href={"/cadastro"}>Não possui conta? Crie a sua.</Link>
-        </div>
-      </Form>
+      <LoginForm onFinish={onFinish} onFinishFailed={onFinishFailed} />
+      <div>
+        <Link href={"/cadastro"}>Não possui conta? Crie a sua.</Link>
+      </div>
     </div>
   );
 };
 
-export default App;
+export default AuthPage;
